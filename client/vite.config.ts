c141307313eb4fa78e957f0f2d78dffca11e576d
Jerson <jerson.ml820@hotmail.com>
2024-11-20 22:49:56 -0400

    wip: se reestructuró proyecto base

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Escuchar en todas las interfaces
+    port: 5173,      // Puerto predeterminado
+    watch: {
+      usePolling: true, // Para garantizar que se detecten cambios en entornos Docker
+    },
+  },
 })
